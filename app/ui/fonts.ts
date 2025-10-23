c2bee92aa86b02e48baf741aef0521b50fe56669
Tzipik <tzipik652@gmail.com>
2025-10-23 14:48:17 +0300

    feat: enhance invoices page with search, pagination, and table components

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,4 +1,26 @@
-import { Inter,Lusitana } from 'next/font/google';
- 
-export const inter = Inter({ subsets: ['latin'] });
-export const lusitana = Lusitana({ weight: ['400', '700'], subsets: ['latin'] });
+import localFont from 'next/font/local';
+
+export const inter = localFont({
+    src: [
+        {
+            path: '../../public/fonts/inter-v20-latin-regular.woff2',
+            weight: '400',
+            style: 'normal',
+        },
+    ],
+});
+
+export const lusitana = localFont({
+    src: [
+        {
+            path: '../../public/fonts/lusitana-v14-latin-regular.woff2',
+            weight: '400',
+            style: 'normal',
+        },
+        {
+            path: '../../public/fonts/lusitana-v14-latin-700.woff2',
+            weight: '700',
+            style: 'normal',
+        },
+    ],
+});
